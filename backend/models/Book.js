342734473317c58602718c021453f36e588ead16
Iya Shchur <iya.schur@gmail.com>
2023-12-19 20:14:02 +0200

    mixed content

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  userId: {type: String, required: true},
+  userId: { type: String, required: true },
   title: { type: String, required: true },
   author: { type: String, required: true },
   imageUrl: { type: String, required: true },
@@ -13,7 +13,7 @@ const bookSchema = mongoose.Schema({
       grade: { type: Number, required: true },
     },
   ],
-  averageRating: { type: Number, required: true}
+  averageRating: { type: Number, required: true },
 });
 
 module.exports = mongoose.model('Book', bookSchema);
